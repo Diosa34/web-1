f622b9bd0eafc7df6c6bee3822577a765a4715d3
Diosa34 <superalex.osa@yandex.ru>
2022-09-03 01:36:35 +0300

    Added functional like .css and .js files

---
+++ validation.js
@@ -0,0 +1,43 @@
+let x = null
+function validationX(){
+    let xElem = document.getElementById("x")
+    let warning = document.getElementById("x-warning")
+    if (isNaN(xElem.value) || xElem.value === "") {
+        warning.innerText = "Координата Х должна быть числом"
+        warning.style.display = "inline-block"
+        x = null
+    } else {
+        warning.style.display = "none"
+        x = xElem.value
+    }
+    buttonDisable()
+}
+
+let selectedCheckbox = null
+function checkboxVal(checkbox){
+    if (checkbox.checked) {
+        if (selectedCheckbox !== null) {
+            selectedCheckbox.checked = false
+        }
+        selectedCheckbox = checkbox
+    } else {
+        selectedCheckbox = null
+    }
+    buttonDisable()
+}
+
+function buttonDisable(){
+    if (x === null || selectedCheckbox === null){
+        document.getElementById("btn1").disabled = true
+        document.getElementById("btn2").disabled = true
+        document.getElementById("btn3").disabled = true
+        document.getElementById("btn4").disabled = true
+        document.getElementById("btn5").disabled = true
+    } else {
+        document.getElementById("btn1").disabled = false
+        document.getElementById("btn2").disabled = false
+        document.getElementById("btn3").disabled = false
+        document.getElementById("btn4").disabled = false
+        document.getElementById("btn5").disabled = false
+    }
+}
